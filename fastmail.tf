3758d6ce8990682bc9f2a292c30870ef3e6b0207
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2021-11-06 16:11:30 -0500

    Switch to using shared module for Fastmail DNS setup

--- fastmail.tf
+++ fastmail.tf
@@ -1,47 +1,5 @@
-resource "dnsimple_record" "apex-primary" {
-  domain   = local.hostname
-  name     = ""
-  value    = local.mx-primary
-  priority = "10"
-  type     = "MX"
-}
+module "fastmail-dns" {
+  source = "github.com/pbyrne/terraform-fastmail-dnsimple"
 
-resource "dnsimple_record" "apex-secondary" {
-  domain   = local.hostname
-  name     = ""
-  value    = local.mx-secondary
-  priority = "20"
-  type     = "MX"
-}
-
-resource "dnsimple_record" "wildcard-primary" {
-  domain   = local.hostname
-  name     = "*"
-  value    = local.mx-primary
-  priority = "10"
-  type     = "MX"
-}
-
-resource "dnsimple_record" "wildcard-secondary" {
-  domain   = local.hostname
-  name     = "*"
-  value    = local.mx-secondary
-  priority = "20"
-  type     = "MX"
-}
-
-resource "dnsimple_record" "apex-spf" {
-  domain = local.hostname
-  name   = ""
-  value  = local.spf
-  type   = "TXT"
-}
-
-resource "dnsimple_record" "dkim" {
-  count = 4
-
-  domain = local.hostname
-  name   = "fm${count.index + 1}._domainkey"
-  value  = "fm${count.index + 1}.byrne.team.dkim.fmhosted.com"
-  type   = "CNAME"
+  apex = "byrne.team"
 }
