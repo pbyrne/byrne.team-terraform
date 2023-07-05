c434f0feb74abbc827b8add462e6873eb0236381
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2023-07-04 20:27:08 -0500

    Fix duplicate resource name
    - I was copying/pasting from the docs and forgot to change the resource name.
    - Also add redirect for the apex domain

--- dnsimple.tf
+++ dnsimple.tf
@@ -1,11 +1,18 @@
-resource "dnsimple_zone_record" "foobar" {
+resource "dnsimple_zone_record" "patrick-redirect" {
   zone_name = var.apex
   name   = "patrick"
   value  = "https://patrickbyrne.net"
   type   = "URL"
 }
 
-resource "dnsimple_zone_record" "foobar" {
+resource "dnsimple_zone_record" "apex-redirect" {
+  zone_name = var.apex
+  name   = ""
+  value  = "https://patrickbyrne.net"
+  type   = "URL"
+}
+
+resource "dnsimple_zone_record" "patrick-bluesky-verification" {
   zone_name = var.apex
   name   = "_atproto.patrick"
   value  = "did=did:plc:krtm6dltju5ikaqwvca24sa5"
