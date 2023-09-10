99df493bef3a71052cbfb3e9816ed93e81b65bfe
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2023-09-10 15:02:36 -0500

    Add DNS for my DO droplet

--- dnsimple.tf
+++ dnsimple.tf
@@ -1,20 +1,27 @@
 resource "dnsimple_zone_record" "patrick-redirect" {
   zone_name = var.apex
-  name   = "patrick"
-  value  = "https://patrickbyrne.net"
-  type   = "URL"
+  name      = "patrick"
+  value     = "https://patrickbyrne.net"
+  type      = "URL"
 }
 
 resource "dnsimple_zone_record" "apex-redirect" {
   zone_name = var.apex
-  name   = ""
-  value  = "https://patrickbyrne.net"
-  type   = "URL"
+  name      = ""
+  value     = "https://patrickbyrne.net"
+  type      = "URL"
 }
 
 resource "dnsimple_zone_record" "patrick-bluesky-verification" {
   zone_name = var.apex
-  name   = "_atproto.patrick"
-  value  = "did=did:plc:krtm6dltju5ikaqwvca24sa5"
-  type   = "TXT"
+  name      = "_atproto.patrick"
+  value     = "did=did:plc:krtm6dltju5ikaqwvca24sa5"
+  type      = "TXT"
+}
+
+resource "dnsimple_zone_record" "droplet" {
+  name      = "droplet"
+  type      = "CNAME"
+  value     = "198.199.84.189" # TODO Get this as output from managing the DigitalOcean Droplet in Terraform
+  zone_name = var.apex
 }
