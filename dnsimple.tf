07242ceb4c3ab729166836fd366ecfd5ac7aaafc
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2023-09-10 15:08:43 -0500

    Actually I don't want it

--- dnsimple.tf
+++ dnsimple.tf
@@ -18,10 +18,3 @@ resource "dnsimple_zone_record" "patrick-bluesky-verification" {
   value     = "did=did:plc:krtm6dltju5ikaqwvca24sa5"
   type      = "TXT"
 }
-
-resource "dnsimple_zone_record" "droplet" {
-  name      = "droplet"
-  type      = "CNAME"
-  value     = "198.199.84.189" # TODO Get this as output from managing the DigitalOcean Droplet in Terraform
-  zone_name = var.apex
-}
